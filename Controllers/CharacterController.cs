5f255bfc268d032f1f793501dd6fd7c67a6e07c4
Felipe Augusto L. C. Magalhães <felipealcmagalhaes@gmail.com>
2023-04-16 23:19:41 -0300

    Test list and Post method to insert data on it

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character{Id = 1, Name = "Sam"}
+        };
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<List<Character>> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+    }
+}
